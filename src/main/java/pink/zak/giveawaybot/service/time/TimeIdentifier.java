b5ee799bfeb070fec10a1fd13065a6fb72efdef5
Zak <shearmanzak@gmail.com>
2020-12-20 19:39:55 +0000

    Update time identifiers

--- src/main/java/pink/zak/giveawaybot/service/time/TimeIdentifier.java
+++ src/main/java/pink/zak/giveawaybot/service/time/TimeIdentifier.java
@@ -2,13 +2,13 @@ package pink.zak.giveawaybot.service.time;
 
 public enum TimeIdentifier {
 
-    SECOND(1000, 9223372036854775L, "second", "seconds", "sec", "s"),
-    MINUTE(60000, 153722867000000L, "minute", "minutes", "min", "m"),
+    SECOND(1000, 9223372036854775L, "second", "seconds", "sec", "secs", "s"),
+    MINUTE(60000, 153722867000000L, "minute", "minutes", "min", "mins", "m"),
     HOUR(3600000, 2562047780000L, "hour", "hours", "h", "hr", "hrs"),
-    DAY(86400000, 106751991000L, "day", "days", "d"),
-    WEEK(604800000, 15250284400L, "week", "weeks", "w"),
-    MONTH(2630016000L, 476571388, "month", "months", "mo"),
-    YEAR(31536000000L, 1305675, "y", "yr", "year", "years");
+    DAY(86400000, 106751991000L, "day", "days", "d", "ds"),
+    WEEK(604800000, 15250284400L, "week", "weeks", "w", "ws"),
+    MONTH(2630016000L, 476571388, "month", "months", "mo", "mos"),
+    YEAR(31536000000L, 1305675, "year", "years", "yr", "yrs", "y");
 
     private final long milliseconds;
     private final long maxAmountOf;
