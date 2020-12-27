99fd7a235b806df00955f3e5fbb23546e35bfc96
Zak <shearmanzak@gmail.com>
2020-12-27 00:24:25 +0000

    Clean commands & fix deadlock on unload server

--- src/main/java/pink/zak/giveawaybot/service/listener/ConsoleListener.java
+++ src/main/java/pink/zak/giveawaybot/service/listener/ConsoleListener.java
@@ -29,10 +29,7 @@ public class ConsoleListener implements Runnable {
             String command = args[0];
             switch (command.toLowerCase()) {
                 case "help":
-                    this.logger.info("help -> Prints this command.");
-                    this.logger.info("reload -> Reloads language values (built embeds require restart).");
-                    this.logger.info("stop -> Stops the bot and saves data.");
-                    this.logger.info("dump -> Creates a debug dump.");
+                    this.helpCommand();
                     break;
                 case "reload":
                     this.bot.reload();
@@ -43,23 +40,10 @@ public class ConsoleListener implements Runnable {
                     }
                     break;
                 case "dump":
-                    long startTime = System.currentTimeMillis();
-                    String fileName = this.heapDump();
-                    this.logger.info("Created your heap dump called " + fileName + " in " + (System.currentTimeMillis() - startTime) + "ms.");
+                    this.dumpCommand();
                     break;
                 case "unload-server":
-                    if (args.length < 2) {
-                        this.logger.info("You must specify a server ID.");
-                        return;
-                    }
-                    ServerCache serverCache = this.bot.getServerCache();
-                    long serverId = Long.parseLong(args[1]);
-                    if (!serverCache.contains(serverId)) {
-                        this.logger.info("The server {} is not cached", serverId);
-                        return;
-                    }
-                    this.logger.info("Invalidating {}", serverId);
-                    serverCache.invalidate(serverId);
+                    this.unloadServerCommand(args);
                     break;
                 default:
                     break;
@@ -67,9 +51,41 @@ public class ConsoleListener implements Runnable {
         }
     }
 
+    private void helpCommand() {
+        this.logger.info("help -> Prints this command.");
+        this.logger.info("reload -> Reloads language values (built embeds require restart).");
+        this.logger.info("stop -> Stops the bot and saves data.");
+        this.logger.info("dump -> Creates a debug dump.");
+    }
+
+    private void dumpCommand() {
+        long startTime = System.currentTimeMillis();
+        String fileName = this.heapDump();
+        this.logger.info("Created your heap dump called " + fileName + " in " + (System.currentTimeMillis() - startTime) + "ms.");
+    }
+
+    private void unloadServerCommand(String[] args) {
+        if (args.length < 2) {
+            this.logger.info("You must specify a server ID.");
+            return;
+        }
+        ServerCache serverCache = this.bot.getServerCache();
+        try {
+            long serverId = Long.parseLong(args[1]);
+            if (!serverCache.contains(serverId)) {
+                this.logger.info("The server {} is not cached", serverId);
+                return;
+            }
+            this.logger.info("Invalidating {}", serverId);
+            serverCache.invalidate(serverId);
+        } catch (NumberFormatException ex) {
+            GiveawayBot.getLogger().error("Input is not a long ({})", args[1]);
+        }
+    }
+
     @SneakyThrows
     private String heapDump() {
-        String fileName = new SimpleDateFormat("yyyy-MM-dd-hh-mm-ss").format(new Date(System.currentTimeMillis())) + " internal dump.hprof";
+        String fileName = new SimpleDateFormat("yyyy-MM-dd-hh-mm-ss").format(new Date(System.currentTimeMillis())) + " dump.hprof";
         ManagementFactory.newPlatformMXBeanProxy(ManagementFactory.getPlatformMBeanServer(),
                 "com.sun.management:type=HotSpotDiagnostic", HotSpotDiagnosticMXBean.class)
                 .dumpHeap(fileName, true);
