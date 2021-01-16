075c7bd99ee8b1b1d19f1e68558c43af12aa3471
Zak <shearmanzak@gmail.com>
2021-01-16 02:54:36 +0000

    Update PremiumCommandTests.java

--- discord/src/test/java/pink/zak/test/giveawaybot/command/console/PremiumCommandTests.java
+++ discord/src/test/java/pink/zak/test/giveawaybot/command/console/PremiumCommandTests.java
@@ -3,7 +3,6 @@ package pink.zak.test.giveawaybot.command.console;
 import pink.zak.giveawaybot.discord.cache.ServerCache;
 
 public class PremiumCommandTests {
-    private final ServerCache serverCache = new ServerCache(null)
 
     public void test() {
 
