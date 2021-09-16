93947294aeec250332828121b1bfb2942ceb0ff0
Zak <shearmanzak@gmail.com>
2021-09-16 19:01:46 +0100

    Fix basic issues

--- src/main/java/pink/zak/giveawaybot/Main.java
+++ src/main/java/pink/zak/giveawaybot/Main.java
@@ -1,11 +1,9 @@
 package pink.zak.giveawaybot;
 
-import java.nio.file.Path;
-
 public class Main {
 
     public static void main(String[] argsArray) {
-        GiveawayBot bot = new GiveawayBot(path -> Path.of("C:\\Users\\shear\\Documents\\Projects\\SmartGiveaways\\smart-giveaway-bot\\build\\libs"));
+        GiveawayBot bot = new GiveawayBot(path -> path);
         bot.load();
     }
 }
